bf4291cd096121a463863e5c9ec968c09633a748
Sion Jeong <sioninone@gmail.com>
2024-07-18 09:50:41 +0900

    real Final
    Good Bye~~

--- Assets/Scripts/GameClear.cs
+++ Assets/Scripts/GameClear.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class GameClear : MonoBehaviour
 {
     PlayerState _playerState;
     public Animator _imageAnim;
     public GameObject gameClear;
+    public bool _gameQuit;
 
     void Awake()
     {
-        _playerState = FindObjectOfType<PlayerState>();        
+        _playerState = FindObjectOfType<PlayerState>();      
+        _gameQuit = false;
+    }
+
+    private void Update()
+    {
+        if(_gameQuit && Input.anyKey)
+        {
+
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#else
+            Application.Quit();
+
+#endif
+        }
     }
 
     public void EnterSun()
@@ -22,6 +41,8 @@ public class GameClear : MonoBehaviour
     public void ActiveGameClear()
     {
         gameClear.SetActive(true);
+        _gameQuit = true;
+
     }
 
 }
